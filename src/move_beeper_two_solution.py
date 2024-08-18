093ea6c3a39adb64b82c45550dd42ddf4996575f
Devraj Agarwal <rajagarwal@hotmail.com>
2024-08-18 15:42:13 -0400

    adding new problem

---
+++ src/move_beeper_two_solution.py
@@ -0,0 +1,64 @@
+from stanfordkarel import *
+
+
+def main():
+    """
+    Task: Go to the beeper. Pickup beeper. Go to the target
+    location and put a beeper. Return home.
+    """
+
+    # 1. Go to beeper
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+
+    # 2. Pick-up beeper
+    pick_beeper()
+
+    # 3. Go to target
+    turn_right()
+    move()
+    turn_right()
+    move()
+    move()
+    move()
+    move()
+
+    # 4. Put beeper
+    put_beeper()
+
+    # 5. Return home
+    turn_around()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_right()
+    move()
+    move()
+    move()
+    turn_around()
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+if __name__ == "__main__":
+    run_karel_program("move_beeper_two_start")
---
+++ worlds/move_beeper_two_end.w
@@ -0,0 +1,22 @@
+Dimension: (7, 7)
+Wall: (1, 2); north
+Wall: (1, 4); north
+Wall: (2, 2); north
+Wall: (2, 4); north
+Wall: (3, 2); north
+Wall: (3, 3); east
+Wall: (3, 4); east
+Wall: (3, 4); north
+Wall: (4, 3); east
+Wall: (4, 4); east
+Wall: (4, 5); east
+Wall: (5, 3); south
+Wall: (5, 5); north
+Wall: (6, 2); north
+Wall: (6, 5); north
+Wall: (7, 3); south
+Wall: (7, 6); south
+Beeper: (2, 5); 0
+Beeper: (6, 6); 1
+Karel: (1, 1); east
+BeeperBag: INFINITY
